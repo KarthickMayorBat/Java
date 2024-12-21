46c7a805322354cab542646e504c6f513a95aaa1
KarthickMayorBat <85236947+KarthickMayorBat@users.noreply.github.com>
2024-12-21 06:03:23 +0000

    Rotate matrix to 90 degree

---
+++ MatrixProblems/RotateMatrix.java
@@ -0,0 +1,66 @@
+public class RotateMatrix {
+    /*
+     * Given a square matrix mat[][] of size n x n. The task is to rotate it by 90 degrees in an anti-clockwise direction without using any extra space. 
+
+Examples:
+
+Input: mat[][] = [[1, 2, 3],
+                [4, 5, 6]
+                [7, 8, 9]]
+Output: Rotated Matrix:
+[3, 6, 9]
+[2, 5, 8]
+[1, 4, 7]
+Input: mat[][] = [[1, 2],
+                [3, 4]]
+Output: Rotated Matrix:
+[2, 4]
+[1, 3]
+     */
+    public static void rotate90AntiClockwise(int[][] mat) {
+        int n = mat.length;
+
+        // Step 1: Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
+            }
+        }
+
+        // Step 2: Reverse each column
+        for (int i = 0; i < n; i++) {
+            for (int j = 0, k = n - 1; j < k; j++, k--) {
+                int temp = mat[j][i];
+                mat[j][i] = mat[k][i];
+                mat[k][i] = temp;
+            }
+        }
+    }
+
+    public static void printMatrix(int[][] mat) {
+        for (int[] row : mat) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] mat = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println("Original Matrix:");
+        printMatrix(mat);
+
+        rotate90AntiClockwise(mat);
+
+        System.out.println("Matrix after 90-degree anticlockwise rotation:");
+        printMatrix(mat);
+    }
+}
